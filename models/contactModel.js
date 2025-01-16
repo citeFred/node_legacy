aa4be6753345eb733408a9e60903aa30d1c2ea31
citeFred <citefred@gmail.com>
2025-01-16 17:16:55 +0900

    refactor : Model과 DB 커넥션 모듈 분리

--- models/contactModel.js
+++ models/contactModel.js
@@ -1,26 +1,4 @@
-const mysql = require('mysql2');
-require('dotenv').config();
-
-// MySQL connection Pool :
-const connectionPool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PW,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    connectionLimit: 10,
-    insecureAuth: true,
-});
-
-// MySQL connection check
-connectionPool.getConnection((err, connection) => {
-    if (err) {
-      console.error('MySQL에 연결 중 에러 발생:', err);
-    } else {
-      console.log('MySQL에 연결되었습니다.');
-      connection.release();
-    }
-});
+const connectionPool = require('./db');
 
 // 데이터 조회
 const getContacts = (callback) => {
